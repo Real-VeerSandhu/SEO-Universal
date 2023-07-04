f10519cc5a066890cfc9391a00a31e6e62a82854
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-04 12:03:31 -0400

    add SEARCH

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,12 +12,12 @@ import { HousingService } from '../housing.service';
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
-    <app-housing-location *ngFor="let housingLocation of housingLocationList"
+    <app-housing-location *ngFor="let housingLocation of filteredLocationList"
     [housingLocation]="housingLocation"></app-housing-location> 
   </section> 
 `, // why [] = "", name references housing-location
@@ -27,7 +27,20 @@ export class HomeComponent {
   housingLocationList: HousingLocation[] = []; // set empty array
   housingService: HousingService = inject(HousingService); // inject data
 
+  filteredLocationList: HousingLocation[] = [];
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => 
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+      );
+  }
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations(); // assign data to correct values
+    this.filteredLocationList = this.housingLocationList;
   } 
 }
